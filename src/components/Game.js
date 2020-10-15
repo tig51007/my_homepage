1c2ae275fa87f196419982d419a4080e3ec0e5d7
tig51007 <jun1678214@gmail.com>
2020-10-15 17:18:04 +0900

    setVer

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,23 +16,23 @@ function Game({winFail,name,img,kill,death,assist,item0,item1,item2,item3,item4,
                         <tr><div className="match" ><td>{mode}<br/><hr/><br/>승리<br/>{time}</td><th>
                           
                         <tr><td><div className="championInfo">
-                          <td><img className="championImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+img+""}/></td>
-                          <td><tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/spell/"+spell1+""}/></th></tr>
-                          <tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/spell/"+spell2+""}/></th></tr></td></div></td></tr>
+                          <td><img className="championImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+img+""}/></td>
+                          <td><tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/spell/"+spell1+""}/></th></tr>
+                          <tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/spell/"+spell2+""}/></th></tr></td></div></td></tr>
                           <tr><td ><div className="championName">{name}</div></td></tr>                      </th> <td>
                         <div className="killDeath">{kill}/{death}/{assist}</div>
-                        </td><td className="theItem"><tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item0+".png"}></img></th>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item1+".png"}></img></td>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item2+".png"}></img></td></tr>
-                        <tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item3+".png"}></img></th>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item4+".png"}></img></td>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item5+".png"}></img></td>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item6+".png"}></img></td></tr></td>
-                        <td className="matchInfo"><tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg1+""}></img>{summoner1}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg6+""}></img>{summoner6}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg2+""}></img>{summoner2}</th> <td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg7+""}></img>{summoner7}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg3+""}></img>{summoner3}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg8+""}></img>{summoner8}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg4+""}></img>{summoner4}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg9+""}></img>{summoner9}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg5+""}></img>{summoner5}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg10+""}></img>{summoner10}</td></tr>
+                        </td><td className="theItem"><tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item0+".png"}></img></th>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item1+".png"}></img></td>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item2+".png"}></img></td></tr>
+                        <tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item3+".png"}></img></th>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item4+".png"}></img></td>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item5+".png"}></img></td>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item6+".png"}></img></td></tr></td>
+                        <td className="matchInfo"><tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg1+""}></img>{summoner1}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg6+""}></img>{summoner6}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg2+""}></img>{summoner2}</th> <td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg7+""}></img>{summoner7}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg3+""}></img>{summoner3}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg8+""}></img>{summoner8}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg4+""}></img>{summoner4}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg9+""}></img>{summoner9}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg5+""}></img>{summoner5}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg10+""}></img>{summoner10}</td></tr>
                         
                        
                         
@@ -43,23 +43,23 @@ function Game({winFail,name,img,kill,death,assist,item0,item1,item2,item3,item4,
                         :
                         <tr><div className="match" style={style}><td>{mode}<br/><hr/><br/>패배<br/>{time}</td><th>
                             <tr><td><div className="championInfo">
-                          <td><img className="championImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+img+""}/></td>
-                          <td colSpan="2" rowSpan="2"><tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/spell/"+spell1+""}/></th></tr>
-                          <tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/spell/"+spell2+""}/></th></tr></td></div></td></tr>
+                          <td><img className="championImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+img+""}/></td>
+                          <td colSpan="2" rowSpan="2"><tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/spell/"+spell1+""}/></th></tr>
+                          <tr><th><img className="spellImg" alt="" src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/spell/"+spell2+""}/></th></tr></td></div></td></tr>
                           <tr><td ><div className="championName">{name}</div></td></tr>                      </th> <td>
                         <div className="killDeath">{kill}/{death}/{assist}</div>
-                        </td><td className="theItem"><tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item0+".png"}></img></th>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item1+".png"}></img></td>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item2+".png"}></img></td></tr>
-                        <tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item3+".png"}></img></th>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item4+".png"}></img></td>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item5+".png"}></img></td>
-                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.6.1/img/item/"+item6+".png"}></img></td></tr></td>
-                        <td className="matchInfo"><tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg1+""}></img>{summoner1}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg6+""}></img>{summoner6}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg2+""}></img>{summoner2}</th> <td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg7+""}></img>{summoner7}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg3+""}></img>{summoner3}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg8+""}></img>{summoner8}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg4+""}></img>{summoner4}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg9+""}></img>{summoner9}</td></tr>
-                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg5+""}></img>{summoner5}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg10+""}></img>{summoner10}</td></tr>
+                        </td><td className="theItem"><tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item0+".png"}></img></th>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item1+".png"}></img></td>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item2+".png"}></img></td></tr>
+                        <tr><th><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item3+".png"}></img></th>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item4+".png"}></img></td>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item5+".png"}></img></td>
+                        <td><img className="itemImg" alt=""src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/item/"+item6+".png"}></img></td></tr></td>
+                        <td className="matchInfo"><tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg1+""}></img>{summoner1}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg6+""}></img>{summoner6}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg2+""}></img>{summoner2}</th> <td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg7+""}></img>{summoner7}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg3+""}></img>{summoner3}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg8+""}></img>{summoner8}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg4+""}></img>{summoner4}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg9+""}></img>{summoner9}</td></tr>
+                        <tr><th><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.21.1/img/champion/"+summonerImg5+""}></img>{summoner5}</th><td><img className="summonerChampion"src={"https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/"+summonerImg10+""}></img>{summoner10}</td></tr>
                         
                        
                         
