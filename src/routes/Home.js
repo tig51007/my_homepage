f9337178bef86f4f6758802bdd16a1baeb5d32d7
tig51007 <jun1678214@gmail.com>
2020-11-02 16:35:15 +0900

    addRankStyle

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,16 @@ import "./Home.css";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import Game from "../components/Game";
+import tier1 from "../tier/Emblem_Iron.png";
+import tier2 from "../tier/Emblem_Bronze.png";
+import tier3 from "../tier/Emblem_Silver.png";
+import tier4 from "../tier/Emblem_Gold.png";
+import tier5 from "../tier/Emblem_Platinum.png";
+import tier6 from "../tier/Emblem_Diamond.png";
+import tier7 from "../tier/Emblem_Master.png";
+import tier8 from "../tier/Emblem_Grandmaster.png";
+import tier9 from "../tier/Emblem_Challenger.png";
+
 
 var count=0;
 
@@ -26,6 +36,7 @@ class Home extends React.Component{
     spell2:[],
     tData:[],
     rankData:[],
+    fRankData:[],
     name:'',
     theName:'',
     hide:true
@@ -84,9 +95,12 @@ fetch('https://newserver51007.herokuapp.com/api/lol12')
 .catch(error =>count=count+1)  
 fetch('https://newserver51007.herokuapp.com/api/myRank')               
 .then(res=>res.json())
-.then(tData=>tData[1]!=null?this.setState({rankData: tData[1]}):this.setState({rankData: ""}))
+.then(tData=>tData[0]!=null?this.setState({rankData: tData[0]}):this.setState({rankData: ""}))
 .catch(error =>count=count+1)
-
+fetch('https://newserver51007.herokuapp.com/api/myRank')               
+.then(res=>res.json())
+.then(tData=>tData[1]!=null?this.setState({fRankData: tData[1]}):this.setState({fRankData: ""}))
+.catch(error =>count=count+1)
 }
 
 
@@ -103,8 +117,69 @@ componentDidMount(){
     
   
  
-    const {hide,name,theName,isLoading,myInfo,myMetch,myGame,chamImg,winFail,killDeath,myItem,otherChamp,otherSummon,spell1,spell2,tData,rankData}= this.state;
+    const {hide,name,theName,isLoading,myInfo,myMetch,myGame,chamImg,winFail,killDeath,myItem,otherChamp,otherSummon,spell1,spell2,tData,rankData,fRankData}= this.state;
+   var tier
+   var tierA
    
+   if(rankData.tier=="IRON"){
+     tier=tier1
+     tierA=1
+   }else if(rankData.tier=="BRONZE"){
+     tier=tier2
+     tierA=2
+   }else if(rankData.tier=="SILVER"){
+     tier=tier3
+     tierA=3
+   }else if(rankData.tier=="GOLD"){
+     tier=tier4
+     tierA=4
+    }else if(rankData.tier=="PLATINUM"){
+     tier=tier5
+     tierA=5
+    }else if(rankData.tier=="DIAMOND"){
+     tier=tier6
+     tierA=6
+    }else if(rankData.tier=="MASTER"){
+     tier=tier7
+     tierA=7
+    }else if(rankData.tier=="GRANDMASTER"){
+      tier=tier8
+      tierA=8
+    }else if(rankData.tier=="CHALLENGER"){
+      tier=tier9
+      tierA=9
+    } 
+    var Ftier 
+    var FtierA
+    if(fRankData.tier=="IRON"){
+      Ftier=tier1
+      FtierA=1
+    }else if(fRankData.tier=="BRONZE"){
+      Ftier=tier2
+      FtierA=2
+    }else if(fRankData.tier=="SILVER"){
+      Ftier=tier3
+      FtierA=3
+    }else if(fRankData.tier=="GOLD"){
+      Ftier=tier4
+      FtierA=4
+     }else if(fRankData.tier=="PLATINUM"){
+      Ftier=tier5
+      FtierA=5
+     }else if(fRankData.tier=="DIAMOND"){
+      Ftier=tier6
+      FtierA=6
+     }else if(fRankData.tier=="MASTER"){
+      Ftier=tier7
+      FtierA=7
+     }else if(fRankData.tier=="GRANDMASTER"){
+       Ftier=tier8
+       FtierA=8
+     }else if(fRankData.tier=="CHALLENGER"){
+       Ftier=tier9
+       FtierA=9
+     }  var swab
+    
     
    
    var sortWinFail=[];
@@ -150,13 +225,16 @@ componentDidMount(){
        {myInfo.name}.gg
        </div>
        </div>
-   <div className="tier" >
-     솔로랭크:{rankData.tier} {rankData.rank} 승:{rankData.wins} 패:{rankData.losses}
-     
-     </div>
+   
      
      
-     </div><span className="test1" >asdg</span><span className="compoGame">
+     </div>{rankData.queueType=="RANKED_FLEX_SR"?<div className="test2"><div className="testContent"><div className="test1" ><img className="tierImg"src={Ftier}/><div className="tierRank1">솔로랭크<br/><a className="tierRank1_A">{fRankData.tier} {fRankData.rank}</a> <br/>{fRankData.wins}승 {fRankData.losses}패<br/>승률{`${Math.floor(fRankData.wins/(fRankData.wins+fRankData.losses)*100)}%`}</div></div></div>
+     <div className="testContent"><div className="test1" ><img className="tierImg"src={tier}/><div className="tierRank1">자유랭크<br/><a className="tierRank1_A">{rankData.tier} {rankData.rank}</a> <br/>{rankData.wins}승 {rankData.losses}패<br/>승률{`${Math.floor(rankData.wins/(rankData.wins+rankData.losses)*100)}%`}</div></div></div></div>:
+     <div className="test2"><div className="testContent"><div className="test1" ><img className="tierImg"src={tier}/><div className="tierRank1">솔로랭크<br/><a className="tierRank1_A">{rankData.tier} {rankData.rank}</a> <br/>{rankData.wins}승 {rankData.losses}패<br/>승률{`${Math.floor(rankData.wins/(rankData.wins+rankData.losses)*100)}%`}</div></div></div>
+     <div className="testContent"><div className="test1" ><img className="tierImg"src={Ftier}/><div className="tierRank1">자유랭크<br/><a className="tierRank1_A">{fRankData.tier} {fRankData.rank}</a> <br/>{fRankData.wins}승 {fRankData.losses}패<br/>승률{`${Math.floor(fRankData.wins/(fRankData.wins+fRankData.losses)*100)}%`}</div></div></div></div>}
+     <div className="test2">
+     <div className="compoGame">
+       
     {imE.map(game=> (
                         <Game
                         name={game.name}
@@ -200,7 +278,7 @@ componentDidMount(){
                         
                        
                         />         
-                      
+                     
      
      ))}
      
@@ -208,7 +286,7 @@ componentDidMount(){
      
   
     
-     </span>
+     </div></div>
    </body>):(<body><div className="notSetting">검색하소</div></body>)}
       </section>
 }
